abbd4e209396de0b9e9f202c1ba20f94fc6907c0
Gilang Gumilar <gilangmlr@gmail.com>
2017-06-28 06:31:00 +0700

    specify db table for PIC model

--- app/PIC.php
+++ app/PIC.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class PIC extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'p_i_cs';
 }
