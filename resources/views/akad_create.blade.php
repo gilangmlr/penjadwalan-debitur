41e92b9eb62a1fb223ae562cde5d2ff455d01b32
Gilang Gumilar <gilangmlr@gmail.com>
2017-06-27 09:47:09 +0700

    add bootstrap-clockpicker

--- resources/views/akad_create.blade.php
+++ resources/views/akad_create.blade.php
@@ -15,7 +15,7 @@
                             <label for="nama-notaris" class="col-md-4 control-label">Nama Notaris</label>
 
                             <div class="col-md-6">
-                                <select id="nama-notaris" type="text" class="form-control" name="nama-notaris" value="{{ old('nama-notaris') }}">
+                                <select id="nama-notaris" type="text" class="form-control" name="id-notaris" value="{{ old('nama-notaris') }}">
                                     @foreach($notaris as $nota)
                                         <option value="{{ $nota->id }}">{{$nota->name}}</option>
                                     @endforeach
@@ -47,7 +47,7 @@
                             <label for="fasilitas" class="col-md-4 control-label">Fasilitas</label>
 
                             <div class="col-md-6">
-                                <select id="fasilitas" type="text" class="form-control" name="fasilitas" value="{{ old('fasilitas') }}">
+                                <select id="fasilitas" type="text" class="form-control" name="id-fasilitas" value="{{ old('fasilitas') }}">
                                     @foreach($fasilitas as $fasil)
                                         <option value="{{ $fasil->id }}">{{$fasil->name}}</option>
                                     @endforeach
@@ -62,10 +62,13 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('plafond') ? ' has-error' : '' }}">
-                            <label for="plafond" class="col-md-4 control-label">Plafond (Rp.)</label>
+                            <label for="plafond" class="col-md-4 control-label">Plafond</label>
 
                             <div class="col-md-6">
-                                <input id="plafond" type="text" class="form-control" name="plafond" value="{{ old('plafond') }}" required autofocus>
+                                <div class="input-group">
+                                    <span class="input-group-addon">Rp.</span>
+                                    <input id="plafond" type="text" class="form-control" name="plafond" value="{{ old('plafond') }}" required autofocus>
+                                </div>
 
                                 @if ($errors->has('plafond'))
                                     <span class="help-block">
@@ -79,7 +82,7 @@
                             <label for="pendamping" class="col-md-4 control-label">Pendamping</label>
 
                             <div class="col-md-6">
-                                <select id="pendamping" type="text" class="form-control" name="pendamping" value="{{ old('pendamping') }}">
+                                <select id="pendamping" type="text" class="form-control" name="id-pendamping" value="{{ old('pendamping') }}">
                                     @foreach($pendamping as $pendam)
                                         <option value="{{ $pendam->id }}">{{$pendam->name}}</option>
                                     @endforeach
@@ -97,7 +100,7 @@
                             <label for="pic" class="col-md-4 control-label">PIC</label>
 
                             <div class="col-md-6">
-                                <select id="pic" type="text" class="form-control" name="pic" value="{{ old('pic') }}">
+                                <select id="pic" type="text" class="form-control" name="id-pic" value="{{ old('pic') }}">
                                     @foreach($pic as $pi)
                                         <option value="{{ $pi->id }}">{{$pi->name}}</option>
                                     @endforeach
@@ -115,7 +118,12 @@
                             <label for="jam-akad-mulai" class="col-md-4 control-label">Jam Akad</label>
 
                             <div class="col-md-6">
-                                <input id="jam-akad-mulai" type="time" class="form-control" name="jam-akad-mulai" value="{{ old('jam-akad-mulai') }}" required>
+                                <div class="input-group clockpicker">
+                                    <input id="jam-akad-mulai" type="text" class="form-control" name="jam-akad-mulai" value="{{ old('jam-akad-mulai') }}" required readonly style="background-color: white; cursor: pointer;">
+                                    <span class="input-group-addon">
+                                        <span class="glyphicon glyphicon-time"></span>
+                                    </span>
+                                </div>
 
                                 @if ($errors->has('jam-akad-mulai'))
                                     <span class="help-block">
@@ -129,7 +137,12 @@
                             <label for="jam-akad-selesai" class="col-md-4 control-label">Jam Akad Selesai</label>
 
                             <div class="col-md-6">
-                                <input id="jam-akad-selesai" type="time" class="form-control" name="jam-akad-selesai" value="{{ old('jam-akad-selesai') }}" required>
+                                <div class="input-group clockpicker">
+                                    <input id="jam-akad-selesai" type="text" class="form-control" name="jam-akad-selesai" value="{{ old('jam-akad-selesai') }}" required readonly style="background-color: white; cursor: pointer;">
+                                    <span class="input-group-addon">
+                                        <span class="glyphicon glyphicon-time"></span>
+                                    </span>
+                                </div>
 
                                 @if ($errors->has('jam-akad-selesai'))
                                     <span class="help-block">
@@ -143,7 +156,7 @@
                             <label for="ruangan" class="col-md-4 control-label">Ruangan</label>
 
                             <div class="col-md-6">
-                                <select id="ruangan" type="text" class="form-control" name="ruangan" value="{{ old('ruangan') }}">
+                                <select id="ruangan" type="text" class="form-control" name="id-ruangan" value="{{ old('ruangan') }}">
                                     @foreach($ruangan as $ruang)
                                         <option value="{{ $ruang->id }}">{{$ruang->name}}</option>
                                     @endforeach
@@ -170,4 +183,40 @@
         </div>
     </div>
 </div>
+@endsection
+
+@section('script')
+<script type="text/javascript">
+    function prependedZeroTime(date) {
+        var H = date.getHours();
+        var HH = H;
+        if (H < 10) HH = "0" + H;
+        var m = date.getMinutes();
+        var mm = m;
+        if (m < 10) mm = "0" + m;
+
+        return HH + ":" + mm;
+    }
+
+    var date = new Date();
+    var formattedTime = prependedZeroTime(date);
+    
+    $("#jam-akad-mulai").val(formattedTime).attr("placeholder", formattedTime);
+    $("#jam-akad-mulai").parent().clockpicker({
+        placement: 'top',
+        align: 'left',
+        donetext: 'Done',
+        default: formattedTime
+    });
+
+    date.setHours(date.getHours() + 1);
+    formattedTime = prependedZeroTime(date);
+    $("#jam-akad-selesai").val(formattedTime).attr("placeholder", formattedTime);
+    $("#jam-akad-selesai").parent().clockpicker({
+        placement: 'top',
+        align: 'left',
+        donetext: 'Done',
+        default: formattedTime
+    });
+</script>
 @endsection
