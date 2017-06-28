a3925cd8b4b4916e47231136a88347d4df8040ff
Gilang Gumilar <gilangmlr@gmail.com>
2017-06-28 07:39:30 +0700

    add jam_akad_mulai and jam_akad_selesai attributes for akad migration

--- database/migrations/2017_06_26_190325_create_akads_table.php
+++ database/migrations/2017_06_26_190325_create_akads_table.php
@@ -21,16 +21,19 @@ class CreateAkadsTable extends Migration
             $table->unsignedInteger('plafond');
             $table->unsignedInteger('pendamping_id');
             $table->unsignedInteger('p_i_c_id');
+            $table->timestamp('jam_akad_mulai');
+            $table->timestamp('jam_akad_selesai');
             $table->unsignedInteger('ruangan_id');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
 
         for ($i = 0; $i < 5; $i++) {
+            $jamMulai = time() + (3600 * $i);
             DB::table('akads')->insert(
                 ['notaris_id' => $i, 'nama_debitur' => 'Debitur ' . $i,
                  'fasilitas_id' => $i, 'plafond' => $i * 1000000,
-                 'pendamping_id' => $i, 'p_i_c_id' => $i,'ruangan_id' => $i,]
+                 'pendamping_id' => $i, 'p_i_c_id' => $i, 'jam_akad_mulai' => $jamMulai, 'jam_akad_selesai' => $jamMulai + 3600, 'ruangan_id' => $i,]
             );
         }
     }
