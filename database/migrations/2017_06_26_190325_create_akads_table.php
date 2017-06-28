b32fec576aeae6459408b5f8718fcef918a65223
Gilang Gumilar <gilangmlr@gmail.com>
2017-06-28 07:19:01 +0700

    add attributes for akad migration

--- database/migrations/2017_06_26_190325_create_akads_table.php
+++ database/migrations/2017_06_26_190325_create_akads_table.php
@@ -15,9 +15,24 @@ class CreateAkadsTable extends Migration
     {
         Schema::create('akads', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('notaris_id');
+            $table->string('nama_debitur');
+            $table->unsignedInteger('fasilitas_id');
+            $table->unsignedInteger('plafond');
+            $table->unsignedInteger('pendamping_id');
+            $table->unsignedInteger('p_i_c_id');
+            $table->unsignedInteger('ruangan_id');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
+
+        for ($i = 0; $i < 5; $i++) {
+            DB::table('akads')->insert(
+                ['notaris_id' => $i, 'nama_debitur' => 'Debitur ' . $i,
+                 'fasilitas_id' => $i, 'plafond' => $i * 1000000,
+                 'pendamping_id' => $i, 'p_i_c_id' => $i,'ruangan_id' => $i,]
+            );
+        }
     }
 
     /**
