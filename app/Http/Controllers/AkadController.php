e42ba40d08d8771974fa6da3538c1253c61a374b
Gilang Gumilar <gilangmlr@gmail.com>
2017-06-28 07:50:34 +0700

    insert akad into database

--- app/Http/Controllers/AkadController.php
+++ app/Http/Controllers/AkadController.php
@@ -9,6 +9,7 @@ use App\Fasilitas;
 use App\Pendamping;
 use App\PIC;
 use App\Ruangan;
+use App\Akad;
 
 class AkadController extends Controller
 {
@@ -36,8 +37,15 @@ class AkadController extends Controller
                                     "pendamping" => $pendamping, "pic" => $pic, "ruangan" => $ruangan]);
     }
 
-    public function crud_create()
+    public function crud_create(Request $request)
     {
-        
+        $all = $request->all();
+        DB::table('akads')->insert(
+                ['notaris_id' => $all['id-notaris'], 'nama_debitur' => $all['nama-debitur'],
+                 'fasilitas_id' => $all['id-fasilitas'], 'plafond' => (int) $all['plafond'],
+                 'pendamping_id' => $all['id-pendamping'], 'p_i_c_id' => $all['id-pic'],
+                 'jam_akad_mulai' => strtotime($all['jam-akad-mulai']),
+                 'jam-akad-selesai' => strtotime($all['jam-akad-selesai']),'ruangan_id' => $i,]
+            );
     }
 }
