525fa4905604582513287b2a210df7de01a19645
Gilang Gumilar <gilangmlr@gmail.com>
2017-07-18 22:19:31 +0700

    install Zizaco/Entrust for Role-based Permissions

--- config/app.php
+++ config/app.php
@@ -180,6 +180,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Zizaco\Entrust\EntrustServiceProvider::class,
+
     ],
 
     /*
@@ -228,6 +230,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
 
     ],
 
